c068e0d1392ab42638bbee4d218e3e42d0457462
Arwa07 <83685833+Arwa07@users.noreply.github.com>
2021-05-12 19:27:13 +0530

    Add files via upload

--- librarianlogin.java
+++ librarianlogin.java
@@ -1,4 +1,4 @@
-package librarymanagement;
+
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -30,6 +30,7 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.CompoundBorder;
 import javax.swing.SwingConstants;
 import java.sql.* ;
+import java.awt.SystemColor;
 public class librarianlogin extends JFrame {
 	ResultSet rs=null;
 	
@@ -58,7 +59,7 @@ public class librarianlogin extends JFrame {
 		
 		setBackground(new Color(255, 255, 255));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 707, 473);
+		setBounds(100, 100, 704, 469);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
@@ -67,35 +68,35 @@ public class librarianlogin extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("ENTER USERNAME");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setForeground(new Color(153, 0, 51));
-		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 15));
-		lblNewLabel.setBounds(224, 129, 187, 17);
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblNewLabel.setBounds(90, 137, 209, 17);
 		contentPane.add(lblNewLabel);
 		
 		JTextField txtuname = new JTextField();
-		txtuname.setBackground(new Color(204, 204, 255));
+		txtuname.setBackground(Color.WHITE);
 		txtuname.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		txtuname.setBounds(238, 157, 136, 20);
+		txtuname.setBounds(320, 134, 136, 20);
 		contentPane.add(txtuname);
 		txtuname.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("ENTER PASSWORD");
+		JLabel lblNewLabel_1 = new JLabel("\r\nENTER PASSWORD");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setForeground(new Color(153, 0, 0));
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 15));
-		lblNewLabel_1.setBounds(224, 182, 187, 22);
+		lblNewLabel_1.setForeground(Color.BLACK);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblNewLabel_1.setBounds(99, 184, 200, 22);
 		contentPane.add(lblNewLabel_1);
 		JPasswordField txtpwd = new JPasswordField();
-		txtpwd.setBackground(new Color(204, 204, 255));
+		txtpwd.setBackground(Color.WHITE);
 		txtpwd.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtpwd.setEchoChar('*');
-		txtpwd.setBounds(238, 206, 136, 20);
+		txtpwd.setBounds(320, 185, 136, 20);
 		contentPane.add(txtpwd);
 		
 		JButton btnNewButton = new JButton("LOGIN");
 		btnNewButton.setForeground(new Color(153, 0, 51));
-		btnNewButton.setBackground(new Color(204, 204, 255));
-		btnNewButton.setIcon(new ImageIcon("C:\\Users\\Sarim\\Pictures\\Saved Pictures\\preferences-system-login-icon.png"));
+		btnNewButton.setBackground(new Color(250, 240, 230));
+		btnNewButton.setIcon(new ImageIcon("C:\\Users\\arwam\\OneDrive\\Documents\\GitHub\\Library-Management-Project\\images\\preferences-system-login-icon.png"));
 		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -104,7 +105,7 @@ public class librarianlogin extends JFrame {
 				
 				if(uname.equals("")||pswd.equals(""))
 				{
-					JOptionPane.showMessageDialog(btnNewButton, "some fields are empty","error",1);
+					JOptionPane.showMessageDialog(btnNewButton, "Some fields are empty","error",1);
 				}else {
 					try {
 						Class.forName("com.mysql.cj.jdbc.Driver");
@@ -119,22 +120,19 @@ public class librarianlogin extends JFrame {
 						{
 							String s1=rs.getString("user_id");
 							String s2=rs.getString("password");
-							JOptionPane.showMessageDialog(btnNewButton, "logined successfully");
+							JOptionPane.showMessageDialog(btnNewButton, "Logged in Successfully");
 							txtuname.setText("");
 							txtpwd.setText("");
 						}
 						else
 						{
-							JOptionPane.showMessageDialog(btnNewButton, " user_id or password not matched");
+							JOptionPane.showMessageDialog(btnNewButton, " User_id or Password are not matched");
 							txtuname.setText("");
 							txtpwd.setText("");
 
-						}
-
-						
+						}	
 						
 						}catch(Exception e1){
-						
 					
 					}
 				}
@@ -146,15 +144,15 @@ public class librarianlogin extends JFrame {
 			
 			}
 		});
-		btnNewButton.setBounds(195, 237, 115, 42);
+		btnNewButton.setBounds(158, 304, 115, 42);
 		contentPane.add(btnNewButton);
 		
 		
 		
 		JCheckBox ckbox = new JCheckBox("show password\r\n");
 		ckbox.setForeground(new Color(153, 0, 51));
-		ckbox.setFont(new Font("Times New Roman", Font.BOLD, 11));
-		ckbox.setBackground(new Color(255, 204, 255));
+		ckbox.setFont(new Font("Times New Roman", Font.BOLD, 18));
+		ckbox.setBackground(SystemColor.info);
 		ckbox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if(ckbox.isSelected())
@@ -168,34 +166,35 @@ public class librarianlogin extends JFrame {
 				}
 			}
 		});
-		ckbox.setBounds(402, 208, 119, 21);
+		ckbox.setBounds(383, 230, 151, 29);
 		contentPane.add(ckbox);
 		
 		JButton btnNewButton_1 = new JButton("CANCEL");
 		btnNewButton_1.setForeground(new Color(153, 0, 51));
-		btnNewButton_1.setBackground(new Color(204, 204, 255));
-		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\Sarim\\Pictures\\Saved Pictures\\Close-2-icon.png"));
+		btnNewButton_1.setBackground(new Color(250, 240, 230));
+		btnNewButton_1.setIcon(new ImageIcon("C:\\Users\\arwam\\OneDrive\\Documents\\GitHub\\Library-Management-Project\\images\\Close-2-icon.png"));
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
 		btnNewButton_1.setFont(new Font("Times New Roman", Font.BOLD, 14));
-		btnNewButton_1.setBounds(333, 237, 115, 42);
+		btnNewButton_1.setBounds(385, 304, 115, 42);
 		contentPane.add(btnNewButton_1);
 		
 		JLabel lblNewLabel_4 = new JLabel("LIBRARIAN LOGIN");
 		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_4.setForeground(new Color(204, 0, 51));
 		lblNewLabel_4.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		lblNewLabel_4.setBounds(147, 67, 374, 51);
+		lblNewLabel_4.setBounds(148, 35, 374, 51);
 		contentPane.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setForeground(new Color(102, 0, 0));
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Sarim\\Pictures\\Saved Pictures\\20191219170611-GettyImages-1152794789.jpeg"));
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\ARWA\\TYBCA\\SEM-VI\\Project\\bg1.PNG"));
 		lblNewLabel_2.setBackground(new Color(204, 102, 204));
-		lblNewLabel_2.setBounds(0, 0, 681, 434);
+		lblNewLabel_2.setBounds(0, 0, 690, 434);
 		contentPane.add(lblNewLabel_2);
 		
 		
